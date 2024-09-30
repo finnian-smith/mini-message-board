441962bec3b4abb28c18afda5783aacf265a083c
finnian-smith <finniansmith01@gmail.com>
2024-09-30 15:00:22 +0100

    Update db queries

--- db/queries.js
+++ db/queries.js
@@ -1,11 +1,9 @@
 import { pool } from "./pool.js";
 
 const db = {
-  // get all messages
   async getAllMessages() {
     try {
       const { rows } = await pool.query("SELECT * FROM messages");
-      console.log("getting all messages");
       return rows;
     } catch (error) {
       console.error("Error fetching messages:", error);
@@ -13,27 +11,24 @@ const db = {
     }
   },
 
-  // insert new message
   async insertNewMessage(text, username) {
     try {
-      //   await pool.query(
-      //     "INSERT INTO messages (text, username, date) VALUES ($1, $2, CURRENT_DATE)",
-      //     [text, username]
-      //   );
-      console.log("inserting new message");
+      await pool.query(
+        "INSERT INTO messages (text, username, date) VALUES ($1, $2, CURRENT_DATE)",
+        [text, username]
+      );
     } catch (error) {
       console.error("Error inserting new message:", error);
       throw new Error("Could not insert new message");
     }
   },
 
-  // get message by id
   async getMessageById(id) {
     try {
-      //   const row = await pool.query("SELECT * FROM messages WHERE id = $1", [
-      //     id,
-      //   ]);
-      console.log("getting message by id");
+      const result = await pool.query("SELECT * FROM messages WHERE id = $1", [
+        id,
+      ]);
+      return result.rows.length > 0 ? result.rows[0] : null;
     } catch (error) {
       console.error("Error fetching message:", error);
       throw new Error("Could not fetch message");
